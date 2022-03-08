40e83fa82766403464beb5dc7a389968479a8963
Supslike <80964103+Supslike@users.noreply.github.com>
2022-03-09 03:03:55 +0800

    Fixed error in mp4 file type

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ from pytube import Playlist, YouTube
 # Asking user for information
 try:
     mode = int(input("\n\nWhat do you want to do?\n\n1. Download a youtube video\n2. Download a youtube playlist\n\nPlease provide the number: "))
-    file_type = int(input("\n\nWhat file type do you want it to be saved?\n\n1. mp4\n2. mp3\n\nPlease provide the number: "))
+    filetype = int(input("\n\nWhat file type do you want it to be saved?\n\n1. mp4\n2. mp3\n\nPlease provide the number: "))
     directory = input("\n\nPlease input the directory: \n\nexample: ./Musics\n\nDirectory: ")
 except:
-    temp = input("\n\nPlease provide a integer as example. (press enter to exit)")
+    input("\n\nPlease provide a integer as example. (press enter to exit)")
 
 # Checking if all information are provided
 try:
     if mode == "":
-        temp = input("\n\nPlease provide a integer as example. (press enter to exit)")
-    if file_type == "":
-        temp = input("\n\nPlease provide a integer as example. (press enter to exit)")
+        input("\n\nPlease provide a integer as example. (press enter to exit)")
+    if filetype == "":
+        input("\n\nPlease provide a integer as example. (press enter to exit)")
 except:
-    temp = input("\n\nPlease provide a integer as example. (press enter to exit)")
+    input("\n\nPlease provide a integer as example. (press enter to exit)")
 
 # Specifying the file type
-if file_type == 1:
+if filetype == 1:
     file_type = ".mp4"
 else:
     file_type = ".mp3"
@@ -30,7 +30,10 @@ def download(video, video_number):
     failed = ""
     print(f"#{video_number} [+] Downloading {video.title}")
     try:
-        video_output = video.streams.filter(only_audio=True).first()
+        if filetype == 1:
+            video_output = video.streams.filter(only_audio=True).first()
+        else:
+            video_output = video.streams.get_highest_resolution()
         downloaded_file = video_output.download(directory)
         base, ext = os.path.splitext(downloaded_file)
         new_file = base + file_type
@@ -70,3 +73,4 @@ elif mode == 2:
         video_number += 1
         failed += download(video=video, video_number=video_number)
     print(f"\n\n[+] DONE DOWNLOADING\n\n[-] Failed downloads\n\n{failed}")
+
